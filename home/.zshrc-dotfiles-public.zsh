08687bdd2780bf656b28c6f88ebfab9ce00c7a75
Maksym Bondarchuk <belkamax05@gmail.com>
2025-06-19 02:06:34 +0100

    latest

---
+++ home/.zshrc-dotfiles-public.zsh
@@ -0,0 +1,5 @@
+echo "public zsh included"
+
+
+#! --- finalize ---
+trace-add "Public dotfiles included"
--- include.sh
+++ include.sh
@@ -2,3 +2,5 @@ __CURRENT_FILE=${(%):-%N}
 __CURRENT_DIR=$(realpath $(dirname $__CURRENT_FILE))
 
 link-home-directory "$__CURRENT_DIR/home" "$HOME"
+
+source-if-exists "$HOME/.zshrc-dotfiles-private.zsh"
