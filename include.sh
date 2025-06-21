1c057cc5ef80b7301d1d51bcb1adf48bf8032598
Maksym Bondarchuk <belkamax05@gmail.com>
2025-06-21 05:40:13 +0100

    latest

--- include.sh
+++ include.sh
@@ -1,10 +1,6 @@
 __CURRENT_FILE=${(%):-%N}
 __CURRENT_DIR=$(realpath $(dirname $__CURRENT_FILE))
 
-link-directory-recursively "$__CURRENT_DIR/home" "$HOME"
+link-directory-recursive "$__CURRENT_DIR/home" "$HOME"
 
-#! Shulker prompt
-clone-and-source "git@github.com:belkamax05/shulker-prompt.git" "$HOME/dev/shulker-prompt" "include.zsh"
-
-#! --- finalize ---
 trace-add "Public dotfiles included"
