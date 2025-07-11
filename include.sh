a895321fdb2c43a26f7a65f6b655a08bd234c613
Maksym Bondarchuk <belkamax05@gmail.com>
2025-07-11 07:11:06 +0100

    latest

--- include.sh
+++ include.sh
@@ -1,6 +1,7 @@
 __CURRENT_FILE=${(%):-%N}
 __CURRENT_DIR=$(realpath $(dirname $__CURRENT_FILE))
 
-link-directory-recursive-cached "$__CURRENT_DIR/home" "$HOME"
+# link-directory-recursive-cached "$__CURRENT_DIR/home" "$HOME"
+echo-debug "$(format-project 'dotfiles-public') linking disabled for now. Enable if needed later"
 
 echo-trace-project "dotfiles-public" "$__CURRENT_DIR"
